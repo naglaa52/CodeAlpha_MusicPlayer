1680845b549e59552288f8054277616f82e82aac
Naglaa Elsayed <120828481+naglaa52@users.noreply.github.com>
2024-07-25 22:01:49 +0300

    musicplayer

---
+++ js/playlist.js
@@ -0,0 +1,44 @@
+let songs = [
+  {
+    name: "Love In The Dark",
+    artist: "Adele",
+    img: "img1",
+    audio: "Love in the Dark"
+  },
+  {
+    name: "No Time To Die",
+    artist: "Bellie Eilish",
+    img: "img2",
+    audio: "No Time To Die"
+  },
+  {
+    name: "I Not The Only One",
+    artist: "Sam Smith",
+    img: "img3",
+    audio: "I'm Not The Only One"
+  },
+  {
+    name: "All Of Me",
+    artist: "John Legend",
+    img: "img4",
+    audio: "All Of Me"
+  },
+  {
+    name: "More Than This",
+    artist: "One Direction",
+    img: "img5",
+    audio: "More Than This"
+  },
+  {
+    name: "Entertainer",
+    artist: "Zayn Malik",
+    img: "img6",
+    audio: "Entertainer"
+  },
+  {
+    name: "Creepin'",
+    artist: "21 Savage, Metro Boomin, and The Weeknd",
+    img: "img7",
+    audio: "Creepin'"
+  },
+]
